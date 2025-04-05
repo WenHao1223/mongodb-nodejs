5985abb90d0b4fb40d6a26ebbb3a1f741069b305
WenHao1223 <wenhaojshs@gmail.com>
2025-04-05 15:10:15 +0800

    Move username

--- atlas_uri.js
+++ atlas_uri.js
@@ -1,4 +1,4 @@
 require("dotenv").config();
 
 module.exports =
-    uri = `mongodb+srv://myAtlasDBUser:${process.env.MONGODB_PASSWORD}@myatlasclusteredu.pzi4jw2.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU`;
+    uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@myatlasclusteredu.pzi4jw2.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU`;
